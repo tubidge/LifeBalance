b16bb1dcc0ef8e5b3b8708c2169f9233268a0248
jessiBramwell <bramibly@gmail.com>
2019-05-19 10:35:01 -0600

    small change

--- public/js/index.js
+++ public/js/index.js
@@ -6,11 +6,16 @@
  */
 
 /**
-  * 
-  * The forms are set up to use: $("#signup input[name=username-or-whatever]")
-  * to target the individual inputs. 
-  * 
-  * We'll need an event listener, and object from the form data and an ajax call? Yeah? Is that all we do here?
+* 
+* The forms are set up to use: $("#signup input[name=username-or-whatever]")
+* to target the individual inputs.
+*
+* We'll need an event listener, and object from the form data and an ajax call? Yeah? Is that all we do here?
+*/  
+
+/**
+* === we will need ===
+* "/api/todos" to get todos on page load
 */
 
 /**
@@ -18,3 +23,20 @@
  * get "/signup" - for the "signup" page
  * get "/login" - for the "login" page
  */
+
+/**
+* .todo-item - on click
+*
+*  id from data="id"
+*  === we will need this put route ===
+* "/api/todos" + id
+*
+*/
+
+/**
+* .delete - on click
+*
+*  id from data="id"
+*  === we will need this delete route ===
+*  "/api/todos/" + id
+*/
