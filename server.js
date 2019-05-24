31cfa9a51cd84e9a3bdbeba26d41848770601fd8
tubidge <tubidge@gmail.com>
2019-05-23 21:37:21 -0600

    adding to yml

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ var syncOptions = { force: true };
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
 if (process.env.NODE_ENV === "test") {
-  syncOptions.force = false;
+  syncOptions.force = true;
 }
 
 // Starting the server, syncing our models ------------------------------------/
