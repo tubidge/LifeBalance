2b20c8c50a249493ca828654df1c6e51b470913b
jessiBramwell <bramibly@gmail.com>
2019-05-31 07:03:43 -0600

    eroute to signin when user isn't logged in

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -5,15 +5,35 @@ exports.signup = function (req, res) {
 
 };
 
-exports.signin = function (req, res) {  
+exports.signin = function (req, res) {
   res.render("signin");
 
 };
 
 exports.dashboard = function (req, res) {
-  res.render("dashboard");
 
+  console.log("log from route");
+  console.log(req.session);
+
+  db.Selection.findAll({
+    include: [{
+      model: db.Task,
+      where: {
+        UserId: req.session.passport.user
+      },
+      required: false
+    }]
+  }).then(function (dataSelect) {
+    console.log(dataSelect);
+    var viewObj = {
+      Selection: dataSelect
+    };
+
+    res.render("index", viewObj);
+
+  });
 };
+
 
 exports.logout = function (req, res) {
   req.session.destroy(function (err) {
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,29 +3,29 @@ var db = require("../models");
 module.exports = function (app) {
 
   // Homepage
-  app.get("/", function (req, res) {
+  // app.get("/", function (req, res) {
 
-    console.log("log from route");
-    console.log(req.session);
+  //   console.log("log from route");
+  //   console.log(req.session);
 
-    db.Selection.findAll({
-      include: [{
-        model: db.Task,
-        where: {
-          UserId: req.session.passport.user
-        },
-        required: false
-      }]
-    }).then(function (dataSelect) {
-      console.log(dataSelect);
-      var viewObj = {
-        Selection: dataSelect
-      };
+  //   db.Selection.findAll({
+  //     include: [{
+  //       model: db.Task,
+  //       where: {
+  //         UserId: req.session.passport.user
+  //       },
+  //       required: false
+  //     }]
+  //   }).then(function (dataSelect) {
+  //     console.log(dataSelect);
+  //     var viewObj = {
+  //       Selection: dataSelect
+  //     };
 
-      res.render("index", viewObj);
+  //     res.render("index", viewObj);
 
-    });
-  });
+  //   });
+  // });
 
 
   // Signup
